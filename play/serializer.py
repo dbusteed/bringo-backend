d69308c110c91b97c27003286857f7b4ff44eed4
dbusteed <busteed.davis@gmail.com>
2019-12-29 09:07:08 -0700

    changed owner field to write_only

--- play/serializer.py
+++ play/serializer.py
@@ -4,8 +4,8 @@ from play.models import Board
 class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
+        extra_kwargs = {'owner': {'write_only': True}}
         fields = ('id', 'name', 'tiles', 'owner')
-
     
     def to_representation(self, instance):
         data = super().to_representation(instance)
